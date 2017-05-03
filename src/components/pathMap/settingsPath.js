82ea5541e98d8e46d93803588cee8c54a7217ff0
Alvin1992 <fanrx_1992@163.com>
2017-05-03 15:14:17 +0800

    增加增加管理员的页面

--- src/components/pathMap/settingsPath.js
+++ src/components/pathMap/settingsPath.js
@@ -3,9 +3,11 @@
  */
 import AddUserGroup from '../settings/right/AddUserGroup.vue'
 import EditUserGroup from '../settings/right/EditUserGroup.vue'
+import AddUser from '../settings/right/AddUser.vue'
 export const settingsChildRouter = [
   {path: '', redirect: 'addusergroup'},
   {path: 'addusergroup', component: AddUserGroup},
   {path: 'editusergroup', component: EditUserGroup},
   {path: 'editusergroup/:id', component: AddUserGroup},
+  {path: 'adduser', component: AddUser},
 ];
---
+++ src/components/settings/right/AddUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="right el-col-20">
+    <el-row :gutter="20">
+      <el-col>
+        <div class="container">
+          test
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
