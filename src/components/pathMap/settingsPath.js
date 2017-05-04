002609a2cba52400f0ed4e34f2c59258f93e6011
Alvin1992 <fanrx_1992@163.com>
2017-05-04 09:17:45 +0800

    初始化后台操作日志页面和管理员列表页面

--- src/components/pathMap/settingsPath.js
+++ src/components/pathMap/settingsPath.js
@@ -5,10 +5,14 @@ import AddUserGroup from '../settings/right/AddUserGroup.vue'
 import EditUserGroup from '../settings/right/EditUserGroup.vue'
 import DoEdit from '../settings/right/DoEdit.vue'
 import AddUser from '../settings/right/AddUser.vue'
+import UserList from '../settings/right/UserList.vue'
+import Logs from '../settings/right/Logs.vue'
 export const settingsChildRouter = [
   {path: '', redirect: 'addusergroup'},
   {path: 'addusergroup', component: AddUserGroup},
   {path: 'editusergroup', component: EditUserGroup},
   {path: 'editusergroup/:id', component: DoEdit},
   {path: 'adduser', component: AddUser},
+  {path: 'userlist', component: UserList},
+  {path: 'logs', component: Logs},
 ];
