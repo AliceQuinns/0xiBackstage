e16e9290850763538b6b9efa0b4d0bb0d8630f6e
tangbiao <alerttangbiao@aliyun.com>
2017-05-05 18:56:22 +0800

    店铺等级-未完成

--- src/components/shop/right/Level.vue
+++ src/components/shop/right/Level.vue
@@ -8,9 +8,9 @@
 
             <!-- 管理 -->
             <el-tab-pane>
+              <span slot="label"><i class="el-icon-date"></i>&nbsp;&nbsp;管理</span><!-- 图标 -->
 
               <!-- 提示语 -->
-              <span slot="label"><i class="el-icon-date"></i>&nbsp;&nbsp;管理</span>
               <p class="topicTags">操作提示</p>
               <ul class="promptMessage">
                 <li><i class="el-icon-share"></i>
@@ -24,34 +24,52 @@
               <!-- 数据表格 -->
               <el-table
                 ref="multipleTable"
-                :data="tableData3"
+                :data="tableData"
                 border
-                tooltip-effect="dark"
                 style="width: 100%"
                 @selection-change="handleSelectionChange">
+
+                <!-- selection -->
                 <el-table-column
                   type="selection"
                   width="55">
                 </el-table-column>
+
+                <!-- ID -->
                 <el-table-column
-                  label="日期"
-                  width="120">
-                  <template scope="scope">{{ scope.row.date }}</template>
+                  label="ID"
+                  width="120"
+                  prop="id">
                 </el-table-column>
+
+                <!-- 名称 -->
                 <el-table-column
                   prop="name"
-                  label="姓名"
-                  width="120">
+                  label="名称">
                 </el-table-column>
+
+                <!-- 描述 -->
                 <el-table-column
-                  prop="address"
-                  label="地址"
-                  show-overflow-tooltip>
+                  prop="desc"
+                  label="描述">
                 </el-table-column>
+
+                <!-- 创建时间 -->
+                <el-table-column
+                  prop="createTime"
+                  label="创建时间">
+                </el-table-column>
+
+                <!-- 状态 -->
+                <el-table-column
+                  prop="status"
+                  label="状态"
+                  width="180">
+                </el-table-column>
+
               </el-table>
               <div style="margin-top: 20px">
-                <el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button>
-                <el-button @click="toggleSelection()">取消选择</el-button>
+                <el-button @click="toggleSelection()">删除</el-button>
               </div>
 
             </el-tab-pane>
@@ -59,14 +77,14 @@
 
             <!-- 添加 -->
             <el-tab-pane label="添加">
-              <span slot="label"><i class="el-icon-star-on"></i>    修改</span>
+              <span slot="label"><i class="el-icon-star-on"></i>&nbsp;&nbsp;修改</span>
               添加
             </el-tab-pane>
 
 
             <!-- 修改 -->
             <el-tab-pane label="修改">
-              <span slot="label"><i class="el-icon-edit"></i>    修改</span>
+              <span slot="label"><i class="el-icon-edit"></i>&nbsp;&nbsp;修改</span>
               修改
             </el-tab-pane>
 
@@ -82,40 +100,52 @@
   export default {
     data() {
       return {
-        tableData3: [{
-          date: '2016-05-03',
+        tableData: [
+          {
+          id: '0',
+          createTime: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-02',
+          desc: '上海市普陀区金沙江路 1518 弄',
+          status: '',
+        }
+        , {
+          createTime: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
+          desc: '上海市普陀区金沙江路 1518 弄'
+        }
+        , {
+          createTime: '2016-05-04',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-01',
+          desc: '上海市普陀区金沙江路 1518 弄'
+        }
+        , {
+          createTime: '2016-05-01',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-08',
+          desc: '上海市普陀区金沙江路 1518 弄'
+        }
+        , {
+          createTime: '2016-05-08',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-06',
+          desc: '上海市普陀区金沙江路 1518 弄'
+        }
+        , {
+          createTime: '2016-05-06',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-07',
+          desc: '上海市普陀区金沙江路 1518 弄'
+        }
+        , {
+          createTime: '2016-05-07',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }],
+          desc: '上海市普陀区金沙江路 1518 弄'
+        }
+        ],
         multipleSelection: []
       }
     },
 
     methods: {
+
+      /* 表格操作 */
       toggleSelection(rows) {
         if (rows) {
           rows.forEach(row => {
@@ -127,14 +157,16 @@
       },
       handleSelectionChange(val) {
         this.multipleSelection = val;
-      }
+      },
+
     }
+
   }
 </script>
 
 <style lang="sass" scoped>
   .topicTags
-    background-color: #d3dce6
+    background-color: #eef1f6
     line-height: 3em
     padding-left: 2em
   .promptMessage
