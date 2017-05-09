0a4e32613e17213b3d9903e68aee3faf96790b2e
Alvin1992 <fanrx_1992@163.com>
2017-05-09 19:17:25 +0800

    店铺管理的页面

--- src/router/pathMap/shopPath.js
+++ src/router/pathMap/shopPath.js
@@ -1,10 +1,12 @@
 import Level from '../../components/shop/right/Level.vue'
 import Cate from '../../components/shop/right/Cate.vue'
 import Template from '../../components/shop/right/Template.vue'
+import Manage from '../../components/shop/right/Manage.vue'
 
 export const shopChildRouter = [
   {path: '', redirect: 'level'},
   {path: 'level', component: Level},
   {path: 'cate', component: Cate},
   {path: 'template', component: Template},
+  {path: 'manage', component: Manage},
 ];
