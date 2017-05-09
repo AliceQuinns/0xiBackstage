733a785136010ee5f1503effd8cb5286f6e11305
Alvin1992 <fanrx_1992@163.com>
2017-05-09 15:38:47 +0800

    增加店铺模板的路由及权限

--- src/router/pathMap/shopPath.js
+++ src/router/pathMap/shopPath.js
@@ -1,8 +1,10 @@
 import Level from '../../components/shop/right/Level.vue'
 import Cate from '../../components/shop/right/Cate.vue'
+import Template from '../../components/shop/right/Template.vue'
 
 export const shopChildRouter = [
   {path: '', redirect: 'level'},
   {path: 'level', component: Level},
   {path: 'cate', component: Cate},
+  {path: 'template', component: Template},
 ];
