615cf22f53c8ffb0e09bae7ada3710c5bc128d61
tangbiao <alerttangbiao@aliyun.com>
2017-05-10 16:16:39 +0800

    手机模板

---
+++ src/components/shop/right/Mtemplate.vue
@@ -0,0 +1,485 @@
+<template>
+  <div class="right el-col-20">
+    <el-row :gutter="20">
+      <el-col>
+        <div class="container">
+          <h3>店铺模板</h3>
+          <el-tabs
+            type="border-card"
+            v-model="editableTabsValue">
+            <!-- 管理选项卡 -->
+            <el-tab-pane
+              name="1"
+              value="1">
+              <!-- 选项卡图标 -->
+              <span slot="label" @click="toggleClickShow">
+                <i class="el-icon-date"></i>&nbsp;&nbsp;管理
+              </span>
+              <!-- 数据表格 -->
+              <el-table
+                ref="multipleTable"
+                :data="tableData"
+                border
+                scopeone="scope"
+                style="width: 100%"
+                @selection-change="handleSelectionChange">
+                <!-- 复选框 -->
+                <el-table-column
+                  type="selection"
+                  width="55">
+                </el-table-column>
+                <!-- 显示顺序 -->
+                <el-table-column
+                  label="显示顺序"
+                  width="120">
+                  <template
+                    scope="scope">
+                    <el-input
+                      placeholder="255"
+                      type="number"
+                      size="small"
+                      prop="displayorder">
+                    </el-input>
+                  </template>
+                </el-table-column>
+                <!-- 模板名称 -->
+                <el-table-column
+                  label="模板名称">
+                  <template
+                    scope="scope"
+                    prop="name">
+                    <img class="name-img">
+                    <span class="name-text">实例</span>
+                  </template>
+                </el-table-column>
+                <!-- 模板风格 -->
+                <el-table-column
+                  prop="style"
+                  label="模板风格">
+                </el-table-column>
+                <!-- 状态 -->
+                <el-table-column
+                  label="状态"
+                  prop="status">
+                </el-table-column>
+                <!-- 创建时间 -->
+                <el-table-column
+                  prop="create_time"
+                  label="创建时间">
+                </el-table-column>
+                <!-- 操作 -->
+                <el-table-column
+                  label="操作"
+                  width="180">
+                  <template
+                    scope="scope">
+                    <!--修改-->
+                    <el-button
+                      icon="edit"
+                      size="small"
+                      @click="clickShow(scope.$index, scope.row)">
+                      修改
+                    </el-button>
+                    <!--删除-->
+                    <el-button
+                      type="danger"
+                      icon="delete"
+                      size="small"
+                      @click="handleDelete(scope.$index, scope.row)">
+                      删除
+                    </el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <!-- 提交 -->
+              <div class="submit-display">
+                <el-button
+                  type="info">
+                  提交
+                </el-button>
+              </div>
+            </el-tab-pane>
+            <!-- 添加--选项卡 -->
+            <el-tab-pane
+              label="添加"
+              name="2"
+              value="2">
+              <span slot="label" @click="toggleClickShow"><i class="el-icon-star-on"></i>&nbsp;&nbsp;添加</span>
+              <!-- 标题 -->
+              <p class="topicTags">添加店铺</p>
+              <div id="">
+                <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="">
+
+                  <el-form-item label="模板名称" prop="name">
+                    <el-col :span="8"><el-input v-model="ruleForm.name"></el-input></el-col>
+                  </el-form-item>
+
+                  <el-form-item label="模板风格" prop="style">
+                    <el-col :span="8"><el-input v-model="ruleForm.style"></el-input></el-col>
+                  </el-form-item>
+
+                  <el-form-item label="模板文件" prop="tempFile" class="tempFileSelection">
+                    <el-select v-model="ruleForm.tempFile" placeholder="请选择模板文件">
+                      <el-option label="数据1" value="1"></el-option>
+                      <el-option label="数据2" value="2"></el-option>
+                    </el-select>
+                  </el-form-item>
+
+                  <el-form-item label="状态" prop="state">
+                    <el-switch v-model="ruleForm.state" on-color="#13ce66" off-color="#ff4949">
+                    </el-switch>
+                  </el-form-item>
+
+                  <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">立即添加</el-button>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                  </el-form-item>
+
+                </el-form>
+              </div>
+            </el-tab-pane>
+            <!-- 修改--选项卡 -->
+            <el-tab-pane
+              label="修改"
+              v-if="displayCondition"
+              name="3"
+              value="3">
+              <span slot="label"><i class="el-icon-edit"></i>&nbsp;&nbsp;修改</span>
+              <p class="topicTags">修改店铺</p>
+              <div>
+                <el-form :model="modifyForm" :rules="rulesModifyForm" ref="modifyForm" label-width="100px"
+                         class="demo-modifyForm">
+
+                  <el-form-item label="模板名称" prop="name">
+                    <el-col :span="8"><el-input v-model="modifyForm.name"></el-input></el-col>
+                  </el-form-item>
+
+                  <el-form-item label="模板风格" prop="style">
+                    <el-col :span="8"><el-input v-model="modifyForm.style"></el-input></el-col>
+                  </el-form-item>
+
+                  <el-form-item label="模板文件" prop="tempFile" class="tempFileSelection">
+                    <el-select v-model="modifyForm.tempFile" placeholder="请选择模板文件">
+                      <el-option label="数据1" value="1"></el-option>
+                      <el-option label="数据2" value="2"></el-option>
+                    </el-select>
+                  </el-form-item>
+
+                  <el-form-item label="状态" prop="status">
+                    <el-switch v-model="stateButtonModifyForm" on-color="#13ce66" off-color="#ff4949">
+                    </el-switch>
+                  </el-form-item>
+
+                  <el-form-item>
+                    <el-button type="primary" @click="submitFormEdit('modifyForm')">提交</el-button>
+                    <el-button @click="resetForm('modifyForm')">重置</el-button>
+                  </el-form-item>
+
+                </el-form>
+              </div>
+            </el-tab-pane>
+          </el-tabs>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import NProgress from 'nprogress'
+  import { addShopLevel, getAllShopsLevel,editShopLevel,getOneShopLevel,deleteShopLevel } from '../../../api/index'
+  import { STATUS_SUCCESS } from '../../../common/consts/index'
+
+  export default {
+    /* 数据 */
+    data() {
+      return {
+        /* 显示顺序 */
+        input: [],
+        /* 复选框 */
+        selection: [],
+        /* 选项卡默认选中 */
+        editableTabsValue: '1',
+        /* 分页 */
+        total: 100,
+        currentPage: 1,
+        /* 选项卡切换 */
+        displayCondition: false,
+        /* 管理 -- 表格 */
+        tableData: [],
+        /* 管理 -- 表格 -- 复选框 */
+        multipleSelection: [],
+        /* 添加 -- 表单 */
+        ruleForm: {
+          name: '',
+          delivery: false,
+          type: [],
+          style: '',
+          tempFile: '',
+          state: true,
+        },
+        /* 修改 -- 表单 */
+        modifyForm: {
+          name: '',
+          delivery: false,
+          type: [],
+          style: '',
+          tempFile: '',
+          state: true,
+        },
+        /* 添加表单 -- 表单验证 */
+        rules: {
+          name: [
+            { required: true, message: '请输入模板名称', trigger: 'blur' },
+            { min: 3, max: 25, message: '长度在 3 到 25 个字符', trigger: 'blur' }
+          ],
+          style: [
+            { required: true, message: '请输入模板风格', trigger: 'blur' },
+            { min: 3, max: 25, message: '长度在 3 到 25 个字符', trigger: 'blur' }
+          ],
+        },
+        /* 修改表单 -- 表单验证 */
+        rulesModifyForm: {
+          name: [
+            { required: true, message: '请输入要修改的模板名称', trigger: 'blur' },
+            { min: 3, max: 25, message: '长度在 3 到 25 个字符', trigger: 'blur' }
+          ],
+          style: [
+            { required: true, message: '请输入要修改的模板风格', trigger: 'blur' },
+            { min: 3, max: 25, message: '长度在 3 到 25 个字符', trigger: 'blur' }
+          ],
+        },
+        /* 添加表单 -- 开关滑块 */
+        stateButton: true,
+        /* 修改表单 -- 开关滑块 */
+        stateButtonModifyForm: true,
+      }
+    },
+    /* 计算属性 */
+    methods: {
+      /* 表格删除按钮 */
+      handleDelete(index, row) {
+        this.$confirm('此操作将永久删除该权限组, 是否继续?', '警告', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          NProgress.start();
+          let rowId = row.id;
+          console.log(row);
+          deleteShopLevel(this.axios, { rowId })
+            .then(response => {
+              let data = response.data;
+              if (data.statusCode === STATUS_SUCCESS) {
+                this.tableData.splice(index, 1);
+                this.$message({
+                  message: '删除成功',
+                  type: 'success'
+                });
+              } else {
+                this.$message({
+                  message: '删除失败',
+                  type: 'error'
+                });
+              }
+              NProgress.done();
+            })
+            .catch(e => {
+              this.$message({
+                message: '出现未知错误，请重试',
+                type: 'error'
+              });
+              console.log(e);
+              NProgress.done();
+            });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      },
+      /* 选项卡的切换 */
+      toggleClickShow: function() {
+        this.displayCondition = false;
+      },
+      /* 获取单条店铺信息 */
+      clickShow: function(index, row){
+        this.displayCondition = !this.displayCondition;
+        this.editableTabsValue = '3';
+        /* 单条店铺信息 */
+        console.log("asydgjgas");
+        NProgress.start();
+        getOneShopLevel(this.axios,row.id)
+          .then(response => {
+            let groups = response.data;
+            if (groups.statusCode === STATUS_SUCCESS) {
+              this.modifyForm = groups.data;
+              /*this.modifyForm.name = groups.data.name;
+               this.modifyForm.desc = groups.data.desc;*/
+              if (groups.data.status == 1){
+                this.stateButtonModifyForm = true;
+              }else if(groups.data.status == 0){
+                this.stateButtonModifyForm = false;
+              }
+              NProgress.done();
+            }
+          })
+          .catch(e => {
+            this.$message({
+              message: '获取数据出错，请重新尝试',
+              type: 'error'
+            });
+            console.log(e);
+            NProgress.done();
+          });
+      },
+      /* 管理 -- 保存选中的数组 */
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      /* 添加 -- 表单 -- 提交 */
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            NProgress.start();
+            addShopLevel(this.axios,{
+              name: this.ruleForm.name,
+              style: this.ruleForm.style,
+              state: this.ruleForm.state ? 1 : 0,
+              tempFile: this.ruleForm.tempFile,
+            })
+              .then(response => {
+                let result = response.data;
+                if (result.statusCode === STATUS_SUCCESS) {
+                  this.$message({
+                    message: '增加店铺成功',
+                    type: 'success'
+                  });
+                } else {
+                  this.$message({
+                    message: '增加失败，请重试',
+                    type: 'info'
+                  });
+                }
+                NProgress.done();
+              })
+              .catch(e => {
+                NProgress.done();
+                this.$message({
+                  message: '出现未知错误，请重试',
+                  type: 'error'
+                });
+              });
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      /* 编辑 -- 表单 -- 提交 */
+      submitFormEdit(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            NProgress.start();
+            addShopLevel(this.axios,{
+              name: this.ruleForm.name,
+              style: this.ruleForm.style,
+              state: this.ruleForm.state ? 1 : 0,
+              tempFile: this.ruleForm.tempFile,
+            })
+              .then(response => {
+                let result = response.data;
+                if (result.statusCode === STATUS_SUCCESS) {
+                  this.$message({
+                    message: '修改店铺成功',
+                    type: 'success'
+                  });
+                } else {
+                  this.$message({
+                    message: '修改失败，请重试',
+                    type: 'info'
+                  });
+                }
+                NProgress.done();
+              })
+              .catch(e => {
+                NProgress.done();
+                this.$message({
+                  message: '出现未知错误，请重试',
+                  type: 'error'
+                });
+              });
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      /* 重置表单 */
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      selectRow(index, row) {
+        console.log(index, row);
+      },
+    },
+    /* 页面加载时请求 */
+    created() {
+      NProgress.start();
+      getAllShopsLevel(this.axios)
+        .then(response => {
+          let groups = response.data;
+          if (groups.statusCode === STATUS_SUCCESS) {
+            this.tableData = groups.data;
+            /* 自定 */
+            let tableData = this.tableData;
+            console.log(tableData[0].status);
+            tableData[0].status = "开启";
+          }
+          NProgress.done();
+        })
+        .catch(e => {
+          this.$message({
+            message: '获取数据出错，请重新尝试',
+            type: 'error'
+          });
+          console.log(e);
+          NProgress.done();
+        });
+    },
+
+  }
+</script>
+
+<style lang="sass" scoped>
+  .topicTags
+    background-color: #eef1f6
+    line-height: 3em
+    padding-left: 2em
+    .promptMessage
+      list-style-type: none
+      padding-left: 1em
+      line-height: 2em
+    .pagination
+      float: right
+    //模板实例
+    .name-img
+      float: left
+      background-color: #eef1f6
+      width:  5em
+      height: 5em
+      margin:
+        top: 0.5em
+        bottom: 0.5em
+    .name-text
+      padding-top: 2em
+      padding-left: 10px
+      display: inline-block
+    //提交按钮
+    .submit-display
+      margin-top: 20px
+    //添加店铺 下拉菜单
+    .tempFileSelection
+      margin-left: 10px
+</style>
