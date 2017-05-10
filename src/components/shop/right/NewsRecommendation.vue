aa207843ea1a7849d5aac541388b3c60a283e3af
tangbiao <alerttangbiao@aliyun.com>
2017-05-10 20:51:40 +0800

    店铺新闻推荐 -- 添加路由,添加Vue文件

---
+++ src/components/shop/right/NewsRecommendation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="right el-col-20">
+    <el-row :gutter="20">
+      <el-col>
+        <div class="container">
+          <h3>店铺新闻推荐位</h3>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
+
+<style lang="sass" scoped>
+
+</style>
