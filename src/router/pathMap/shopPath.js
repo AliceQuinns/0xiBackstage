44f12dd1ebfee1b70169f98ef294c6254e5a5103
tangbiao <alerttangbiao@aliyun.com>
2017-05-10 16:15:54 +0800

    增加路由

--- src/router/pathMap/shopPath.js
+++ src/router/pathMap/shopPath.js
@@ -1,10 +1,12 @@
 import Level from '../../components/shop/right/Level.vue'
 import Cate from '../../components/shop/right/Cate.vue'
 import Template from '../../components/shop/right/Template.vue'
+import Mtemplate from '../../components/shop/right/Mtemplate.vue'
 
 export const shopChildRouter = [
   {path: '', redirect: 'level'},
   {path: 'level', component: Level},
   {path: 'cate', component: Cate},
   {path: 'template', component: Template},
+  {path: 'mtemplate', component: Mtemplate},
 ];
