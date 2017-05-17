2d3d0122ac58638cfda21b4de32644b5fef3987f
Alvin1992 <fanrx_1992@163.com>
2017-05-17 14:19:14 +0800

    分类管理页面

---
+++ src/components/product/right/CateManage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="right el-col-20">
+    <el-row :gutter="20">
+      <el-col>
+        <div class="container">
+          <h3>分类管理</h3>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/router/pathMap/productPath.js
+++ src/router/pathMap/productPath.js
@@ -3,9 +3,11 @@
  */
 import ProductList from '../../components/product/right/ProductList.vue'
 import ProductEvaluation from '../../components/product/right/ProductEvaluation.vue'
+import CateManage from '../../components/product/right/CateManage.vue'
 
 export let productChildRouter = [
   {path: '', redirect: 'productlist'},
   {path: 'productlist', component: ProductList},
   {path: 'productevaluation', component: ProductEvaluation},
+  {path: 'catemanage', component: CateManage},
 ];
