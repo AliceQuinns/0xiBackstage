0a20837ef5bf0ebd7a81c160e0fb2a5a5cd617ac
Alvin1992 <fanrx_1992@163.com>
2017-05-22 21:42:42 +0800

    增加拦截器，可以一次性格式化传送的数据；增加axios的认证属性，保证session的统一；

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
+import qs from 'qs'
 import router from './router/index'
 import store from './store/index'
 import ElementUI from 'element-ui'
@@ -7,11 +8,19 @@ import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 import './common/sass/index.sass'
 let instance = axios.create({
-  // baseURL: 'http://192.168.1.146:8080/',
   headers : {
     'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-  }
+  },
+  withCredentials: true,
 });
+// 拦截request对数据进行编码
+instance.interceptors.request.use((config) => {
+  config.data = qs.stringify(config.data);
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 Vue.use(ElementUI);
 Vue.prototype.axios = instance;
 
