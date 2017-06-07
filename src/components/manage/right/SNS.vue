09fc0887446676540d21e0c26f668a0f8d7f4716
Alvin1992 <fanrx_1992@163.com>
2017-06-07 19:17:03 +0800

    SNS页面结构完成

---
+++ src/components/manage/right/SNS.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="right el-col-20">
+    <el-row :gutter="20">
+      <el-col>
+        <div class="container">
+          <h3>SNS</h3>
+          <el-table
+            ref="multipleTable"
+            :data="tableData"
+            border
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange">
+            <el-table-column
+              type="selection"
+              width="55">
+            </el-table-column>
+            <el-table-column
+              label="图片"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="姓名"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="详情"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              label="时间"
+              width="300">
+              <template scope="scope">{{ scope.row.date }}</template>
+            </el-table-column>
+          </el-table>
+          <div style="margin-top: 20px" class="clearfix">
+            <el-button @click="deleteItem" type="danger">删除</el-button>
+            <el-pagination
+              class="pagination"
+              @current-change="handleCurrentChange"
+              :current-page.sync="currentPage"
+              :page-size="10"
+              layout="prev, pager, next, jumper"
+              :total="total">
+            </el-pagination>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [],
+        multipleSelection: [],
+        currentPage: 1,
+        total: 10,
+      }
+    },
+
+    methods: {
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      handleCurrentChange(val) {
+        console.log(val);
+      },
+      deleteItem() {},
+    }
+  };
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/router/pathMap/managePath.js
+++ src/router/pathMap/managePath.js
@@ -4,6 +4,9 @@
 import { getDefaultPath } from '../../common/js/util'
 let defaultPath = getDefaultPath(6, window.localStorage.getItem('powers'));
 
+import SNS from '../../components/manage/right/SNS.vue'
+
 export let manageChildRouter = [
   {path: '', redirect: defaultPath},
+  { path: 'sns', component: SNS, meta: {requirePower: true}},
 ];
